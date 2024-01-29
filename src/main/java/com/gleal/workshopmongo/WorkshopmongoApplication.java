0377bd76999f101a48613e8e1aec98363f46e231
Gabriel Leal <gabryellows@gmail.com>
2024-01-29 11:47:28 -0300

    primeiro commit do projeto

---
+++ src/main/java/com/gleal/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.gleal.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopmongoApplication.class, args);
+    }
+
+}
