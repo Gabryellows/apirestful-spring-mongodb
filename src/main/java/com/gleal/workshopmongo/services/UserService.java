0da7301cf729daf5f94f875229d4b8478452047e
Gabriel Leal <gabryellows@gmail.com>
2024-01-29 16:14:28 -0300

    feat: obtendo um usuário por id

--- src/main/java/com/gleal/workshopmongo/services/UserService.java
+++ src/main/java/com/gleal/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.gleal.workshopmongo.services;
 
 import com.gleal.workshopmongo.domain.User;
 import com.gleal.workshopmongo.repository.UserRepository;
+import com.gleal.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +15,15 @@ public class UserService {
     private UserRepository repo;
     public List<User> findAll() {
        return repo.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repo.findById(id).orElse(null);
+
+        if(user == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+
+        return user;
     }
 }
